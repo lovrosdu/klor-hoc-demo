71bd4a414397d7c74859e9ce9092ab62844c5c16
lovrosdu <lugovic@imada.sdu.dk>
2024-09-18 05:56:50 +0200

    Update demo

--- src/klor_hoc_demo/core.clj
+++ src/klor_hoc_demo/core.clj
@@ -9,20 +9,26 @@
 ;;; Starting Out
 ;;;
 ;;; - Defining a choreography
+;;; - Typed DSL
+;;; - Location polymorphism
+;;; - Role expressions
 ;;; - Concurrency model
 ;;; - Simulator
-;;; - Communication basics
-;;; - Macroexpansion
 
 (defchor simple-print [A B] (-> B) []
   (A (println "Hello!"))
+  (B (println "World!"))
   (B 123))
 
 (comment
   @(simulate-chor simple-print)
   )
 
-;;; ---
+;;; Communicating
+;;;
+;;; - Communication basics
+;;; - Projection via macroexpansion
+;;; - Calling Clojure
 
 (defchor simple-move [A B] (-> A B) [x]
   (A->B x))
@@ -31,16 +37,12 @@
   @(simulate-chor simple-move 42)
   )
 
-;;; ---
-
 (defchor remote-invoke [A B] (-> B A A) [f x]
   (B->A (f (A->B x))))
 
 (comment
   @(simulate-chor remote-invoke inc 42)
   )
-
-;;; ---
 
 (defchor remote-apply [A B] (-> B A A) [f xs]
   (B->A (B (apply f (A->B xs)))))
@@ -49,14 +51,37 @@
   @(simulate-chor remote-apply + [1 2 3])
   )
 
-;;; ---
+;;; RPC
 
 (defchor rpc [A B] (-> A A A) [name xs]
   (let [var (B (resolve (A->B name)))]
     (remote-apply [A B] (B @var) xs)))
 
 (comment
-  @(simulate-chor rpc '+ [1 2 3])
+  (defn my+ [& args]
+    (apply + args))
+
+  @(simulate-chor rpc 'my+ [1 2 3])
+  )
+
+(comment
+  (def rpc-port 7889)
+
+  (def rpc-server
+    (future
+      (with-server [ssc {:port rpc-port}]
+        (loop []
+          (println "Listening on" (str (.getLocalAddress ssc)))
+          (with-accept [ssc sc]
+            (println "Got client" (str (.getRemoteAddress sc)))
+            (play-role (wrap-sockets {:role 'B} {'A sc} :log true)
+                       rpc))
+          (recur)))))
+
+  (with-client [sc {:host "127.0.0.1" :port rpc-port}]
+    (println "Connected to server" (str (.getRemoteAddress sc)))
+    (play-role (wrap-sockets {:role 'A} {'B sc} :log true)
+               rpc 'my+ [1 2 3]))
   )
 
 ;;; Sharing Knowledge
@@ -68,16 +93,26 @@
 (defchor simple-copy [A B] (-> A #{A B}) [x]
   (A=>B x))
 
+(comment
+  @(simulate-chor simple-copy 42)
+  )
+
+(defchor maybe-inc [A B] (-> A B) [x]
+  (if (A=>B (A (= (rand-int 2) 0)))
+    (B (inc (A->B x)))
+    (B (println "Nothing!"))))
+
+(comment
+  @(simulate-chor maybe-inc 42)
+  )
+
 (defchor remote-map [A B] (-> B A A) [f xs]
   (if (A=>B (A (empty? xs)))
     (A nil)
     (A (cons (remote-invoke [A B] f (first xs))
              (remote-map [A B] f (next xs))))))
 
-(defchor maybe-inc [A B] (-> A B) [x]
-  (if (A=>B (A (= (rand-int 2) 0)))
-    (B (inc (A->B x)))
-    (B (println "Nothing!"))))
+;;; Authentication
 
 (defn ask [prompt]
   (print prompt)
@@ -95,40 +130,45 @@
     (C :error)))
 
 (comment
+  (def get-token-ports
+    {:C->S 7890
+     :A->S 7891
+     :C->A 7892})
+
   (def get-token-server
     (future
-      (with-server [ssc1 :port 7889]
-        (with-server [ssc2 :port 7890]
-          (with-accept [ssc2 sc2]
-            (println "Got client" (str (.getRemoteAddress sc2)))
-            (loop []
-              (println "Listening on" (str (.getLocalAddress ssc1)))
-              (println "Listening on" (str (.getLocalAddress ssc2)))
-              (with-accept [ssc1 sc1]
-                (println "Got client" (str (.getRemoteAddress sc1)))
-                (play-role (wrap-sockets {:role 'S} {'C sc1 'A sc2} :log true)
-                           get-token))
-              (recur)))))))
-
-  (def get-token-authenticator
-    (future
-      (with-server [ssc :port 7891]
-        (with-client [sc2 :host "127.0.0.1" :port 7890]
-          (println "Connected to" (str (.getRemoteAddress sc2)))
+      (with-server [ssc1 {:port (get-token-ports :A->S)}
+                    ssc2 {:port (get-token-ports :C->S)}]
+        (println "Listening on" (str (.getLocalAddress ssc1)))
+        (with-accept [ssc1 sc1]
+          (println "Got authenticator" (str (.getRemoteAddress sc1)))
           (loop []
-            (println "Listening on" (str (.getLocalAddress ssc)))
-            (with-accept [ssc sc1]
-              (println "Got client" (str (.getRemoteAddress sc1)))
-              (play-role (wrap-sockets {:role 'A} {'C sc1 'S sc2} :log true)
+            (println "Listening on" (str (.getLocalAddress ssc2)))
+            (with-accept [ssc2 sc2]
+              (println "Got client" (str (.getRemoteAddress sc2)))
+              (play-role (wrap-sockets {:role 'S} {'A sc1 'C sc2} :log true)
                          get-token))
             (recur))))))
 
-  (with-client [sc1 :host "127.0.0.1" :port 7889]
-    (println "Connected to" (str (.getRemoteAddress sc1)))
-    (with-client [sc2 :host "127.0.0.1" :port 7891]
-      (println "Connected to" (str (.getRemoteAddress sc2)))
-      (play-role (wrap-sockets {:role 'C} {'S sc1 'A sc2} :log true)
-                 get-token #(hash-set :password (str/trim (ask "PW: "))))))
+  (def get-token-authenticator
+    (future
+      (with-client [sc1 {:host "127.0.0.1" :port (get-token-ports :A->S)}]
+        (println "Connected to server" (str (.getRemoteAddress sc1)))
+        (with-server [ssc {:port (get-token-ports :C->A)}]
+          (loop []
+            (println "Listening on" (str (.getLocalAddress ssc)))
+            (with-accept [ssc sc2]
+              (println "Got client" (str (.getRemoteAddress sc2)))
+              (play-role (wrap-sockets {:role 'A} {'S sc1 'C sc2} :log true)
+                         get-token))
+            (recur))))))
+
+  (with-client [sc1 {:host "127.0.0.1" :port (get-token-ports :C->S)}
+                sc2 {:host "127.0.0.1" :port (get-token-ports :C->A)}]
+    (println "Connected to server" (str (.getRemoteAddress sc1)))
+    (println "Connected to authenticator" (str (.getRemoteAddress sc2)))
+    (play-role (wrap-sockets {:role 'C} {'S sc1 'A sc2} :log true)
+               get-token #(hash-map :password (str/trim (ask "PW: ")))))
   )
 
 ;;; Returning Multiple Values
@@ -139,31 +179,47 @@
 (defn modpow [base exp mod]
   (.modPow (biginteger base) (biginteger exp) (biginteger mod)))
 
-(defchor exchange-key-1 [A B] (-> #{A B} #{A B} A B [A B]) [g p sa sb]
+(defchor exchange-key [A B] (-> #{A B} #{A B} A B [A B]) [g p sa sb]
   (pack (A (modpow (B->A (B (modpow g sb p))) sa p))
         (B (modpow (A->B (A (modpow g sa p))) sb p))))
 
 (comment
-  ;; Example from <https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange#Cryptographic_explanation>.
-  @(simulate-chor exchange-key-1 5 23 4 3)
+  ;; <https://en.wikipedia.org/wiki/Diffie–Hellman_key_exchange>
+  @(simulate-chor exchange-key 5 23 4 3)
   )
 
-(defchor secure-1 [A B] (-> A B) [x]
-  (unpack [[k1 k2] (exchange-key-1 [A B] 5 23 (A 4) (B 3))]
+(defchor secure [A B] (-> A B) [x]
+  (unpack [[k1 k2] (exchange-key [A B] 5 23 (A 4) (B 3))]
     (B (.xor k2 (A->B (A (.xor k1 (biginteger x))))))))
 
 (comment
-  @(simulate-chor secure-1 42)
+  @(simulate-chor secure 42)
   )
 
-(defchor exchange-key-2 [A B] (-> #{A B} #{A B} A B #{A B}) [g p sa sb]
-  (agree! (A (modpow (B->A (B (modpow g sb p))) sa p))
-          (B (modpow (A->B (A (modpow g sa p))) sb p))))
+(comment
+  (def secure-port 7893)
+
+  (def secure-server
+    (future
+      (with-server [ssc {:port secure-port}]
+        (loop []
+          (println "Listening on" (str (.getLocalAddress ssc)))
+          (with-accept [ssc sc]
+            (println "Got client" (str (.getRemoteAddress sc)))
+            (println "Got number"
+                     (play-role (wrap-sockets {:role 'B} {'A sc} :log true)
+                                secure)))
+          (recur)))))
+
+  (with-client [sc {:host "127.0.0.1" :port secure-port}]
+    (println "Connected to server" (str (.getRemoteAddress sc)))
+    (play-role (wrap-sockets {:role 'A} {'B sc} :log true) secure 42))
+  )
 
 ;;; Higher-Order
 ;;;
 ;;; - Choreographies as values
-;;; - Future work for type inference
+;;; - Type inference
 ;;; - Avoid auxiliary roles, defer to the tutorial/reference
 
 (defchor chain [A B C] (-> (-> B C) (-> A B) A C) [g f x]
@@ -175,12 +231,23 @@
          (chor (-> A B) [x] (A->B (A (* x 10))))
          (A 41)))
 
+(comment
+  @(simulate-chor chain-test)
+  )
+
 (defchor compose [A B C] (-> (-> B C) (-> A B) (-> A C | B)) [g f]
   (chor (-> A C) [x] (g (f x))))
 
+(defchor compose-test [A B C] (-> (-> A C | B)) []
+  (compose [A B C]
+           (chor (-> B C) [x] (B->C (B (+ x 10))))
+           (chor (-> A B) [x] (A->B (A (* x 10))))))
+
+(comment
+  @(simulate-chor compose-test)
+  )
+
 ;;; Escaping the Simulator
-;;;
-;;; - Tic-Tac-Toe over TCP
 
 (def ttt-syms
   '[x o])
@@ -240,22 +307,23 @@
           board' (ttt-place board loc (get ttt-syms idx))]
       (ttt-play [B A] board' (- 1 idx)))))
 
+(defchor ttt-start [A B] (-> #{A B}) []
+  (ttt-play [A B] (ttt-board) 0))
+
 (comment
-  @(simulate-chor ttt-play (ttt-board) 0)
+  (def ttt-port 7894)
 
   (def ttt-server
     (future
-      (with-server [ssc :port 7889]
+      (with-server [ssc {:port ttt-port}]
         (loop []
           (println "Listening on" (str (.getLocalAddress ssc)))
           (with-accept [ssc sc]
             (println "Got client" (str (.getRemoteAddress sc)))
-            (play-role (wrap-sockets {:role 'A} {'B sc} :log true)
-                       ttt-play (ttt-board) 0))
+            (play-role (wrap-sockets {:role 'A} {'B sc} :log true) ttt-start))
           (recur)))))
 
-  (with-client [sc :host "127.0.0.1" :port 7889]
+  (with-client [sc {:host "127.0.0.1" :port ttt-port}]
     (println "Connected to" (str (.getRemoteAddress sc)))
-    (play-role (wrap-sockets {:role 'B} {'A sc} :log true)
-               ttt-play (ttt-board) 0))
+    (play-role (wrap-sockets {:role 'B} {'A sc} :log true) ttt-start))
   )
